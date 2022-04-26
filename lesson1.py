5674699e8fb0cdb912acae0e12b289d2cd2f41c7
Ar21am <arampoghosyan64@gmail.com>
2022-04-26 09:42:22 +0400

    test

---
+++ lesson1.py
@@ -0,0 +1,11 @@
+# s = 'a,2,b,5,c,8,a,4,e,11'
+# s = s.split(',')
+# dict1 = {}
+# for i in range(0, len(s),2):
+#     key = s[i]
+#     value = int(s[i+1])
+#     if key in dict1:
+#         dict1[key] += value
+#     else:
+#         dict1[key] = value
+# print(dict1)
